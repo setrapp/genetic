8da4196d57ef134005611a24a004fceda4ad12c3
Sam Trapp <trapp.s.e@gmail.com>
2014-03-23 03:26:20 -0400

    Created track, car, and stats. Currently the car is user controlled and the stats of top speed, acceleration, and handling are generated randomly upon start.

---
+++ racer/Assets/Scripts/Car.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+	// Car Stats
+	public int topSpeed;
+	public int acceleration;
+	public int handling;
+	public int statPoolSize;
+	public int statMin;
+	public int statMax;
+
+	// Stat Conversions
+	public float topSpeedScale = 1;
+	public float accelerationScale = 1;
+	public float handlingBrakeScale = 1;
+	public float handlingTurnScale = 1;
+
+	// Car Physics
+	public Vector3 velocity = new Vector3();
+	public float onTrackFriction = 0.99f;
+	public float offTrackFriction = 0.95f;
+
+
+	void Update () {
+		// Accelerate
+		if (Input.GetAxis("Vertical") > 0) {
+			velocity += Vector3.up * (acceleration * accelerationScale * Time.deltaTime);
+			if (velocity.sqrMagnitude > (topSpeed * topSpeed) * (topSpeedScale * topSpeedScale)) {
+				velocity = Vector3.up * topSpeed * topSpeedScale;
+			}
+		}
+		// Deccelerate
+		else if (Input.GetAxis("Vertical") < 0) {
+			velocity -= Vector3.up * (handling * handlingBrakeScale * Time.deltaTime);
+			if (Vector3.Dot(velocity, Vector3.up) < 0) {
+				velocity = Vector3.zero;
+			}
+		}
+
+		// Friction
+		if (IsOnTrack()) {
+			velocity *= onTrackFriction;
+		} else {
+			velocity *= offTrackFriction;
+		}
+
+		// Turn
+		if (Input.GetAxis("Horizontal") != 0) {
+			transform.Rotate(0, 0, handling * handlingTurnScale * -Input.GetAxis("Horizontal") * Time.deltaTime);
+		}
+
+		// Apply velocity
+		transform.Translate(velocity * Time.deltaTime);
+	}
+
+	private bool IsOnTrack() {
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity)) {
+			if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Slow Terrain")) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ racer/Assets/Scripts/StatsController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsController : MonoBehaviour {
+	// Car
+	public Car car;
+	public int topSpeed;
+	public int acceleration;
+	public int handling;
+
+	// GUI Display
+	public GUIText topSpeedText;
+	public GUIText accelerationText;
+	public GUIText handlingText;
+
+	// Random
+	private System.Random random;
+	public int statSeed;
+
+	void Start() {
+		topSpeed = car.topSpeed;
+		acceleration = car.acceleration;
+		handling = car.handling;
+		ApplyStats();
+
+		random = new System.Random(statSeed);
+		// TODO Randomize on generation.
+		RandomizeStats();
+		ApplyStats();
+	}
+
+	public void RandomizeStats() {
+		// Generate random stats that does not exceed stat pool.
+		topSpeed = car.statMin + random.Next(0, car.statMax - car.statMin);
+		acceleration = car.statMin + random.Next(0, (car.statMax - car.statMin));
+		if (topSpeed + acceleration + car.statMin > car.statPoolSize) {
+			int statFix = ((topSpeed + acceleration + car.statMin) - car.statPoolSize) / 2;
+			topSpeed -= statFix;
+			acceleration -= statFix;
+		}
+		handling = car.statPoolSize - topSpeed - acceleration;
+	}
+
+	public void ApplyStats() {
+		car.topSpeed = topSpeed;
+		car.acceleration = acceleration;
+		car.handling = handling;
+		
+		topSpeedText.text = "" + topSpeed;
+		accelerationText.text = "" + acceleration;
+		handlingText.text = "" + handling;
+	}
+}
